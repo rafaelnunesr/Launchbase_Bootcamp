1645c6464d63d779d7e322deef5b07c8ade40196
Rafael Rios <rafaelrafssk@hotmail.com>
2020-02-12 19:51:26 -0300

    create.njk created

--- Aulas/Semana_02/module_04/routes.js
+++ Aulas/Semana_02/module_04/routes.js
@@ -9,6 +9,14 @@ routes.get('/instructors', function(req, res) {
     return res.render('instructors/index')
 })
 
+routes.get('/instructors/create', function(req, res) {
+    return res.render('instructors/create')
+})
+
+routes.post('/instructors', function(req, res) {
+    return res.send('recebido') // apenas para visualizarmos que foi recebido os dados
+})
+
 routes.get('/member', function(req, res) {
     return res.render('members')
 })
