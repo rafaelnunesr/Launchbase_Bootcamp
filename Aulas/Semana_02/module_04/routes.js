54e8627554c0b2804a937d647e06570c2b7e754c
Rafael Rios <rafaelrafssk@hotmail.com>
2020-02-12 21:57:01 -0300

    new files

--- Aulas/Semana_02/module_04/routes.js
+++ Aulas/Semana_02/module_04/routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = express.Router()
+const instructors = require('./instructors')
 
 routes.get('/', function(req, res) {
     return res.redirect('/instructors')
@@ -13,23 +14,6 @@ routes.get('/instructors/create', function(req, res) {
     return res.render('instructors/create')
 })
 
-routes.post('/instructors', function(req, res) {
-
-    const keys = Object.keys() // Object e uma funcao que cria um objeto
-                               // Object.keys retorna um array com todas as chaves do body
-
-    for (key of keys)
-    {
-        if (req.body[key] == '') // req.body[key]  e o mesmo que req.body.name,                          //por exemplo
-        {
-            return res.send('Please, fill all the fields!')
-        }
-    }
-    return res.send(req.body) // apenas para visualizarmos que foi recebido os dados
-})
-
-routes.get('/member', function(req, res) {
-    return res.render('members')
-})
+routes.post('/instructors', instructors.post) // instructors.post e o nome da funcao exportada pelo instructors.js
 
 module.exports = routes
