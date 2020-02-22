01efaeddd0301a1693f6cc7a0a48e68e336317fe
Rafael Rios <rafaelrafssk@hotmail.com>
2020-02-21 23:59:11 -0300

    first commit

--- Desafios/Semana_02/Modulo_04.01/routes.js
+++ Desafios/Semana_02/Modulo_04.01/routes.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const teachers = require('./teachers')
 const routes = express.Router()
 
 routes.get('/', function(req, res){
-    return res.render('index')
+    return res.redirect('/teachers')
 })
+
+routes.get('/teachers', function(req, res){
+    return res.render('teachers/index')
+})
+
+routes.get('/teachers/create', function(req, res){
+    return res.render('teachers/create')
+})
+
+routes.post('/teachers', teachers.post)
 
 module.exports = routes
