b6549ba962295ac300287f532fc27f57162bdf7c
Rafael Rios <rafaelrafssk@hotmail.com>
2020-02-25 19:49:28 -0300

    created

---
+++ Desafios/Semana_02/Modulo_04/routes.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const teachers = require('./teachers')
+const routes = express.Router()
+
+routes.get('/', function(req, res){
+    return res.redirect('/teachers')
+})
+
+routes.get('/teachers', function(req, res){
+    return res.render('teachers/index')
+})
+
+routes.get('/teachers/create', function(req, res){
+    return res.render('teachers/create')
+})
+
+routes.post('/teachers', teachers.post)
+
+routes.get('/teachers/:id', teachers.show)
+
+routes.get('/teachers/:id/edit', teachers.edit)
+
+module.exports = routes
