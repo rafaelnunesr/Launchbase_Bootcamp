1b6a9fbce3d1eb9857626042baf87639751ca327
Rafael Nunes <42895506+rafaelnunesr@users.noreply.github.com>
2020-03-04 13:11:13 -0300

    Update recipes.js

--- Desafios/Semana_02/foodfy/desafio/controllers/recipes.js
+++ Desafios/Semana_02/foodfy/desafio/controllers/recipes.js
@@ -1,14 +1,26 @@
 const data = require('../data.json')
+const most_accessed = require('../most-accessed')
 
-//index
+// Public
 exports.index = function(req, res){
-    return res.render('index', {recipes: data.recipes})
+    return res.render('index', {recipes: most_accessed})
 }
 
-exports.recipes = function(req, res){
-    return res.render('recipes', {recipe: data.recipes})
+exports.recipes = function(req, res) {
+    const id = req.query.id
+    if (id === undefined) {
+        return res.render('recipes', {recipe: data.recipes})
+    }
+    const recipe = data.recipes[id]
+    return res.render('recipes', { item: recipe })
 }
 
-exports.admin_index = function(req, res){
-    return res.render('admin/index')
+exports.about = function(req, res){
+    return res.render('about')
+}
+
+// Admin
+
+exports.admin = function(req, res){
+    return res.render('admin/admin', {recipes: data.recipes})
 }
