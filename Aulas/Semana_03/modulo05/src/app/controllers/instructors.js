b2aa70554b7280dd7671376af39a62e06a32f7e4
Rafael Rios <rafaelrafssk@hotmail.com>
2020-03-10 22:29:48 -0300

    banco de dados configurado

--- Aulas/Semana_03/modulo05/src/app/controllers/instructors.js
+++ Aulas/Semana_03/modulo05/src/app/controllers/instructors.js
@@ -1,10 +1,13 @@
+const Instructor = require('../models/Instructor')
 const { age, date } = require('../../lib/utils')
-const db = require('../../config/db')
 
 module.exports = {
     index(req, res){
-        return res.render('instructors/index')
+        Instructor.all(function(instructors){
+            return res.render('instructors/index', { instructors })
+        })
     },
+
     create(req, res){
         return res.render('instructors/create')
     },
@@ -19,39 +22,31 @@ module.exports = {
             }
         }
 
-        const query = `
-            INSERT INTO instructors(
-                name,
-                avatar_url,
-                gender,
-                services,
-                birth,
-                created_at
-            ) VALUES ($1, $2, $3, $4, $5, $6)
-            RETURNING id
-        `
-            
-        const values = [
-            req.body.name,
-            req.body.avatar_url,
-            req.body.gender,
-            req.body.services,
-            date(req.body.birth).iso,
-            date(date.now()).iso,
-        ]
-        
-        db.query(query, values, function(err, results){
-            if(err) return res.send('Database Error')
-            
-            return res.redirect(`/instructors/${results.rows[0].id}`)
+        Instructor.create(req.body, function(instructor){
+            return res.redirect(`/instructors/${instructor.id}`)
         })
 
     },
     show(req, res){
-        return
+        Instructor.find(req.params.id, function(instructor){
+            if (!instructor) return res.send('Instructor not found!')
+
+            instructor.age = age(instructor.birth)
+            instructor.services = instructor.services.split(',')
+
+            instructor.created_at = date(instructor.created_at).format
+
+            return res.render('instructors/show', { instructor })
+        })
     },
     edit(req, res){
-        return
+        Instructor.find(req.params.id, function(instructor){
+            if (!instructor) return res.send('Instructor not found!')
+
+            instructor.birth = date(instructor.birth).iso
+
+            return res.render('instructors/edit', { instructor })
+        })
     },
     put(req, res){
         const keys = Object.keys(req.body) 
@@ -63,9 +58,13 @@ module.exports = {
             }
         }
 
-        return
+        Instructor.update(req.body, function(){
+            return res.redirect(`/instructors/${req.body.id}`)
+        })
     },
     delete(req, res){
-        return
+        Instructor.delete(req.body.id, function(){
+            return res.redirect(`/instructors`)
+        })
     }
 }
--- Aulas/Semana_03/modulo05/src/app/controllers/members.js
+++ Aulas/Semana_03/modulo05/src/app/controllers/members.js
@@ -1,9 +1,13 @@
+const Member = require('../models/Member')
 const { age, date } = require('../../lib/utils')
 
 module.exports = {
     index(req, res){
-        return res.render('members/index')
+        Member.all(function(members){
+            return res.render('members/index', { members })
+        })
     },
+
     create(req, res){
         return res.render('members/create')
     },
@@ -18,15 +22,28 @@ module.exports = {
             }
         }
 
-
-        return
+        Member.create(req.body, function(member){
+            return res.redirect(`/members/${member.id}`)
+        })
 
     },
     show(req, res){
-        return
+        Member.find(req.params.id, function(member){
+            if (!member) return res.send('Member not found!')
+
+            member.birth = date(member.birth).birthDay
+
+            return res.render('members/show', { member })
+        })
     },
     edit(req, res){
-        return
+        Member.find(req.params.id, function(member){
+            if (!member) return res.send('Member not found!')
+
+            member.birth = date(member.birth).iso
+
+            return res.render('members/edit', { member })
+        })
     },
     put(req, res){
         const keys = Object.keys(req.body) 
@@ -38,9 +55,13 @@ module.exports = {
             }
         }
 
-        return
+        Member.update(req.body, function(){
+            return res.redirect(`/members/${req.body.id}`)
+        })
     },
     delete(req, res){
-        return
+        Member.delete(req.body.id, function(){
+            return res.redirect(`/members`)
+        })
     }
 }
