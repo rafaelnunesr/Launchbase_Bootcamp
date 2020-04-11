1a15f4ec7c4b31a112a21e52803bca8e4fe5d0d1
Rafael Rios <rafaelrafssk@hotmail.com>
2020-04-11 14:43:00 -0300

    nunjucks info

--- Desafios/Semana_03/foodfy/new_challenge/src/app/controllers/admin.js
+++ Desafios/Semana_03/foodfy/new_challenge/src/app/controllers/admin.js
@@ -1,17 +1,79 @@
+//const Chef = require('../models/Chef')
+
 module.exports = {
     index(req, res){
-        return res.render('admin/index')
+        const pageInfo = {
+            default_title: 'Gerenciar Receitas',
+            first_button: {
+                link: '/admin/chefs/new_chef',
+                description: 'Novo Chef'
+            },
+            second_button: {
+                link: '/admin/recipes/create',
+                description: 'Nova receita'
+            }
+        }
+
+        return res.render('admin/index', {pageInfo})
     },
     create(req, res){
-        return res.render('admin/create')
+
+        const pageInfo = {
+            default_title: 'Criar nova receita',
+            first_button: {
+                link: '/admin/recipes',
+                description: 'Salvar'
+            }
+        }
+        return res.render('admin/create', {pageInfo})
     },
     recipes(req, res) {
-        return res.render('admin/recipes')
+        const pageInfo = {
+            default_title: 'Gerenciar Receitas',
+            first_button: {
+                link: '/admin/recipes/create',
+                description: 'Nova receita'
+            }
+        }
+
+        return res.render('admin/recipes', {pageInfo})
     },
     chefs(req, res){
-        return res.render('admin/chefs')
+
+        const pageInfo = {
+            default_title: 'Gerenciar Chefs',
+            first_button: {
+                link: '/admin/chefs/new_chef',
+                description: 'Novo Chef'
+            }
+        }
+
+        return res.render('admin/chefs', {pageInfo})
     },
     createChef(req, res){
-        return res.render('admin/new_chef')
+        const pageInfo = {
+            default_title: 'Criar Novo Chef',
+            first_button: {
+                link: '/admin/chefs',
+                description: 'Salvar'
+            }
+        }
+
+        return res.render('admin/new_chef', {pageInfo})
+    },
+    postChef(req, res){
+        const keys = Object.keys(req.body)
+
+        for (key of keys){
+            if (req.body[key] == ''){
+                return res.send("Please, fill all the fields!")
+            }
+        }
+
+        /*
+        Chefs.create(req.body, function(chef){
+            return res.redirect(`/admin/chefs/${chef.id}`)
+        })
+        */
     }
 }
