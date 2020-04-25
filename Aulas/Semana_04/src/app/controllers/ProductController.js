accfe01481e9e416ad2d7b729e770bee1f0582c4
Rafael Rios <rafaelrafssk@hotmail.com>
2020-04-25 13:15:40 -0300

    edit finished

--- Aulas/Semana_04/src/app/controllers/ProductController.js
+++ Aulas/Semana_04/src/app/controllers/ProductController.js
@@ -1,5 +1,6 @@
 const Category = require('../../app/models/Category')
 const Product = require('../../app/models/Product')
+const { formatPrice } = require('../../lib/utils')
 
 module.exports = {
     create(req, res){
@@ -29,9 +30,22 @@ module.exports = {
         let results = await Product.create(req.body)
         const productId = results.rows[0].id
 
+        return res.redirect(`/products/${productId}/edit`)
+
+    },
+    async edit(req, res){
+
+        let results = await Product.find(req.params.id)
+        const product = results.rows[0]
+
+        if(!product) return res.send('Product not found!')
+
+        product.old_price = formatPrice(product.old_price)
+        product.price = formatPrice(product.price)
+
         results = await Category.all()
         const categories = results.rows
 
-        return res.render('products/create.njk', { productId, categories })
+        return res.render('products/edit.njk', { product, categories })
     }
 }
