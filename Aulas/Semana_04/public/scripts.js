1fe3fbc4b01bb1a4eba33e06dd5feaa4cc58de1e
Rafael Rios <rafaelrafssk@hotmail.com>
2020-05-02 12:45:09 -0300

    photo-preview

--- Aulas/Semana_04/public/scripts.js
+++ Aulas/Semana_04/public/scripts.js
@@ -13,3 +13,66 @@ const Mask = {
         }).format(value/100)
     }
 }
+
+const PhotosUpload = {
+    preview: document.querySelector('#photos-preview'),
+    uploadLimit: 6,
+    handleFileInput(event){
+        const { files: fileList } = event.target
+        
+        if (PhotosUpload.hasLimit(event)) return 
+
+        Array.from(fileList).forEach(file => {
+            const reader = new FileReader()
+
+            reader.onload = () => {
+                const image = new Image() // <img />
+                image.src = String(reader.result)
+
+                const div = PhotosUpload.getContainer(image)
+
+                PhotosUpload.preview.appendChild(div)
+            }
+
+            reader.readAsDataURL(file)
+        })
+    },
+    hasLimit(event){
+        const { uploadLimit } = PhotosUpload
+        const { files: fileList } = event.target
+
+        if (fileList.length > uploadLimit){
+            alert(`Envie no máximo ${uploadLimit} fotos.`)
+            event.preventDefault()
+            return true
+        }
+
+        return false
+    },
+    getContainer(image) {
+        const div = document.createElement('div')
+        div.classList.add('photo')
+
+        div.onclick = PhotosUpload.removePhoto
+
+        div.appendChild(image)
+
+        div.appendChild(PhotosUpload.getRemoveButton())
+
+        return div
+    },
+    getRemoveButton(){
+        const button = document.createElement('i')
+        button.classList.add('material-icons')
+        button.innerHTML = "close"
+        return button
+    },
+    removePhoto(event) {
+        const photoDiv = event.target.parentNode // busca o pai do elemento i, no caso a div
+        const photosArray = Array.from(PhotosUpload.preview.children) // array das photos
+
+        const index = photosArray.indexOf(photoDiv)
+
+        photoDiv.remove()
+    }
+}
