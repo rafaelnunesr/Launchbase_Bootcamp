de871ad4135d17bcb74b415492c7d9b14a257a44
Rafael Rios <rafaelrafssk@hotmail.com>
2020-05-04 21:56:41 -0300

    error deleting fotos

--- Aulas/Semana_04/src/app/controllers/ProductController.js
+++ Aulas/Semana_04/src/app/controllers/ProductController.js
@@ -68,9 +68,26 @@ module.exports = {
         const keys = Object.keys(req.body)
 
         for (key of keys){
-            if(req.body[key] == ''){
+            if(req.body[key] == '' && key != 'removed_files'){
                 return res.send('Please, fill all the fields!')
             }
+        }
+
+        if(req.files.length != 0){
+            const newFilesPromise = req.files.map(file => 
+                File.create({...file, product_id: req.body.id}))
+
+            await Promise.all(newFilesPromise)
+        }
+
+        if(req.body.removed_files){
+            const removedFiles = req.body.removed_files.split(',')
+            const lastIndex = removedFiles.length - 1
+            removedFiles.splice(lastIndex, 1)
+
+            const removedFilesPromise = removedFiles.map(id => File.delete(id))
+
+            await Promise.all(removedFilesPromise)
         }
 
         req.body.price = req.body.price.replace(/\D/g, "")
