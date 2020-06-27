6fe8df013be364e26ded63da439aca03491e48ba
Rafael Nunes <rafaelrafssk@hotmail.com>
2020-06-27 12:33:40 -0300

    submodel finished

--- Aulas/Semana_05/src/app/controllers/UserController.js
+++ Aulas/Semana_05/src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User')
 const { formatCep, formatCpfCnpj } = require('../../lib/utils')
+const user = require('../validators/user')
 
 module.exports = {
     registerForm(req, res){
@@ -7,14 +8,14 @@ module.exports = {
     },
     async show(req, res){
 
-        const { userId: id } = req.session
-        const user = await User.findOne( { where: {id} } )
+        const { user } = req
 
-        if (!user) return res.render('user/register', {
-            error: 'Usuário não encontrado!'
-        })
-
+        console.log(user.cpf_cnpj)
+        console.log(typeof user.cpf_cnpj)
+        console.log('_________________')
         user.cpf_cnpj = formatCpfCnpj(user.cpf_cnpj)
+        console.log(user.cep)
+        console.log(typeof user.cep)
         user.cep = formatCep(user.cep)
 
         return res.render('user/index', { user })
@@ -28,5 +29,34 @@ module.exports = {
         return res.redirect('/users')
 
 
+    },
+    async update(req, res){
+        try {
+            let { user } = req
+            let { name, email, cpf_cnpj, cep, address } = req.body
+
+            cpf_cnpj = cpf_cnpj.replace(/\D/g, '')
+            cep = cep.replace(/\D/g, '')
+
+            await User.update(user.id, {
+                name,
+                email,
+                cpf_cnpj,
+                cep,
+                address
+            })
+
+            return res.render('user/index', {
+                user: req.body,
+                success: 'Conta atualizada com sucesso!'
+            })
+
+        }catch(err){
+            console.error(err)
+            return res.render('user/index', {
+                user: req.body,
+                error: 'Algum erro aconteceu!'
+            })
+        }
     }
 }
