8c8c3636cc9da208030903947d6e5e0c32d1693a
Rafael Nunes <42895506+rafaelnunesr@users.noreply.github.com>
2020-07-13 12:49:38 -0300

    Update users.js

--- Desafios/Semana_05/foodfy/src/routes/users.js
+++ Desafios/Semana_05/foodfy/src/routes/users.js
@@ -6,6 +6,7 @@ const UserController = require('../app/controller/UserController')
 const UserValidator = require('../app/validators/user')
 const SessionValidator = require('../app/validators/session')
 const SessionController = require('../app/controller/SessionController')
+const { isLoggedRedirect, onlyUsers, onlyAdminUsers } =  require('../app/middlewares/session')
 
 //login / logout
 routes.get('/login', SessionController.login)
@@ -17,16 +18,16 @@ routes.get('/password-reset', SessionController.resetForm)
 routes.post('/password-reset', SessionValidator.reset, SessionController.reset)
 
 // Rotas de perfil de um usuário logado
-routes.get('/profile', ProfileController.index) // Mostrar o formulário com dados do usuário logado
-routes.get('/profile/:id', ProfileController.edit) // Mostrar o formulário com dados do usuário logado
-routes.put('/profile', SessionValidator.editUser,ProfileController.put)// Editar o usuário logado
+routes.get('/profile', isLoggedRedirect, ProfileController.index) // Mostrar o formulário com dados do usuário logado
+routes.get('/profile/:id', onlyUsers, ProfileController.edit) // Mostrar o formulário com dados do usuário logado
+routes.put('/profile', onlyUsers, SessionValidator.editUser,ProfileController.put)// Editar o usuário logado
 
 // Rotas que o administrador irá acessar para gerenciar usuários
-routes.get('/users', UserController.list) //Mostrar a lista de usuários cadastrados
-routes.get('/user/create', UserController.create)
-routes.post('/users', UserValidator.post, UserController.post) //Cadastrar um usuário
-routes.get('/users/:id', UserValidator.edit, UserController.edit)
-routes.put('/users', UserValidator.put, UserController.put) // Editar um usuário
+routes.get('/users', onlyUsers, UserController.list) //Mostrar a lista de usuários cadastrados
+routes.get('/user/create', onlyUsers, onlyAdminUsers, UserController.create)
+routes.post('/users', onlyUsers, onlyAdminUsers, UserValidator.post, UserController.post) //Cadastrar um usuário
+routes.get('/users/:id', onlyUsers, UserValidator.edit, UserController.edit)
+routes.put('/users', onlyUsers, UserValidator.put, UserController.put) // Editar um usuário
 //routes.delete('/users', UserController.delete) // Deletar um usuário
 
 module.exports = routes
