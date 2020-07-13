4e9d8d44803596fb58ff623d10e1d79dcacc2db1
Rafael Nunes <42895506+rafaelnunesr@users.noreply.github.com>
2020-07-13 12:50:26 -0300

    Create session.js

---
+++ Desafios/Semana_05/foodfy/src/app/middlewares/session.js
@@ -0,0 +1,39 @@
+const User = require('../models/User')
+
+function onlyUsers(req, res, next){
+    if(!req.session.userId){
+        return res.render('admin/login/index', {
+            error: 'Você precisa estar logado para acessar esta sessão.'
+        })
+    }
+
+    next()
+}
+
+function isLoggedRedirect(req, res, next){
+    if(req.session.userId){
+        return res.redirect('/admin')
+    }
+}
+
+async function onlyAdminUsers(req, res, next){
+    
+    const id = req.session.userId
+    const user = await User.findUser({ where: { id } })
+
+    console.log(user)
+
+    if (!user.is_admin){
+        return res.render('admin/users/users', {
+            error: 'Desculpe, você não tem permissões de administrador.'
+        })
+    }
+
+    next()
+}
+
+module.exports =  {
+    onlyUsers,
+    isLoggedRedirect,
+    onlyAdminUsers
+}
