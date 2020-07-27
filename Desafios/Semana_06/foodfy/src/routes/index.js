3c86f8cb12f2ead2eeb01577a978baae3ed0599d
Rafael Nunes <42895506+rafaelnunesr@users.noreply.github.com>
2020-07-27 16:44:19 -0300

    Update index.js

--- Desafios/Semana_06/foodfy/src/routes/index.js
+++ Desafios/Semana_06/foodfy/src/routes/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const routes = express.Router()
 
 const public = require('./public')
+const loginSession = require('./LoginSession')
 const admin = require('./admin')
 const users = require('./users')
 
 routes.use(public)
+routes.use('/login', loginSession)
 routes.use('/admin', admin)
 routes.use('/admin', users)
 
